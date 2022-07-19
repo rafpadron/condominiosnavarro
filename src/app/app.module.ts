023c9524b1d2a6157a12603f47ff315520cb4cd8
Rafael Padron <rafaelpadron0212@gmail.com>
2022-07-19 12:48:59 -0400

    Arreglado del bug del menu movil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LayoutModule } from '@angular/cdk/layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,7 @@ import { LogoComponent } from './components/logo/logo.component';
     CardsComponent,
     ItemIconComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { faPhoneSquareAlt } from '@fortawesome/free-solid-svg-icons';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  constructor(public breakpointObserver:BreakpointObserver) {}
   hideMenu = true;
-  ngOnInit(): void {}
+  ngOnInit(){
+    this.breakpointObserver
+    .observe(['(max-width: 1080px)'])
+    .subscribe((state: BreakpointState) => {
+      if (state.matches) {
+        this.hideMenu = false;
+      } else {
+        this.hideMenu = true;
+      }
+    });
+  }
 }
